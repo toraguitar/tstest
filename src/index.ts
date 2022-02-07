cb0ab7464ff530b59c4420f4d2b5483bd2a57e49
工藤 景虎 <kagetora_kudo@waku-2.com>
2022-02-07 10:26:54 +0900

    設計図途中まで

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import World from "./world";
 
-const root = document.getElementById("root");
-const targetElement = document.querySelector('[data-insert-text="target"]');
-const world = new World("2022年も頑張りましょう");
-world.sayHello(root);
-world.openHelloWindow(targetElement);
