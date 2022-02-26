57fe78e30ffc54d9104aeda1e06881256978a7c7
工藤 景虎 <kagetora_kudo@waku-2.com>
2022-02-26 18:31:22 +0900

    スムーススクロールの実装、スタイルと背景画像の設定

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-const toggleAccordion = (): void => {
-    const triggerButtons: NodeListOf<Element> = document.querySelectorAll('[data-accordion-toggle="trigger"]');
-    const targetAreas: NodeListOf<Element> = document.querySelectorAll('[data-accordion-toggle="target"]');
-    if (triggerButtons.length === 0 || targetAreas.length === 0) return;
-    for(let i = 0; i < triggerButtons.length; i++){
-        triggerButtons[i].addEventListener("click", () => {
-            const targetHeight = targetAreas[i].scrollHeight;
-            targetAreas[i].classList.toggle(`is-opened${targetHeight}`);
+const smoothScroll = (): void => {
+    const triggerTitleList = document.querySelectorAll('[data-smooth-scroll-trigger]');
+    if (triggerTitleList.length === 0) return;
+    for (let i = 0; i < triggerTitleList.length; i++) {
+        triggerTitleList[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            const triggerDataName = triggerTitleList[i].getAttribute('data-smooth-scroll-trigger');
+            const targetAreaTitle = document.querySelector(`[data-smooth-scroll-target="${triggerDataName}"]`);
+            const targetAreaPosition = targetAreaTitle!.getBoundingClientRect().top;
+            const currentScrollNum = window.scrollY;
+            const target = targetAreaPosition + currentScrollNum
+            window.scrollTo({
+                top: target,
+                behavior: 'smooth',
+            });
         });
     }
-
 };
 window.addEventListener("DOMContentLoaded", () => {
-    toggleAccordion();
+    smoothScroll();
 });
