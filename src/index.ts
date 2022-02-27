4d481b1829cf4aef555e38c2541cbdf9095f507b
工藤 景虎 <kagetora_kudo@waku-2.com>
2022-02-27 18:11:34 +0900

    画像とスタイルを追加

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,31 @@ const smoothScroll = (): void => {
         });
     }
 };
+const setCarouselWidth = (): void => {
+    const carouselSlide: HTMLElement | null = document.querySelector('.pokeIntroduce__carouselList');
+    const carouselSlides = document.querySelectorAll('.pokeIntroduce__carouselList');
+    const slideWidth = carouselSlide!.offsetWidth;
+    const slideLength = carouselSlides.length;
+    const slideAreaWidth = slideWidth * slideLength;
+    const carouselSlideArea: HTMLElement | null = document.querySelector('.pokeIntroduce__carouselArea');
+    carouselSlideArea!.style.width = `${slideAreaWidth}px`;
+};
+const setClickButton = (): void => {
+    const carouselSlideArea: HTMLElement | null = document.querySelector('.pokeIntroduce__carouselArea');
+    const carouselSlides = document.querySelectorAll('.pokeIntroduce__carouselList');
+    const slideLength = carouselSlides.length;
+    const slideButtons = document.querySelectorAll('[data-slide-carousel]');
+    for (let i = 0; i < slideButtons.length; i++) {
+        slideButtons[i].addEventListener('click', () => {
+            const slideAreaWidth = carouselSlideArea?.scrollWidth;
+            const slideWidth = slideAreaWidth! / slideLength;
+            console.log(slideWidth);
+        })
+    }
+};
+
 window.addEventListener("DOMContentLoaded", () => {
     smoothScroll();
+    setCarouselWidth();
+    setClickButton();
 });
